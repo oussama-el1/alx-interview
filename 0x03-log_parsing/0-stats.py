36aef98c49312267e5c3b9521147d6e96b079546
oussama-el1 <elhadraoui284@gmail.com>
2024-05-20 21:30:49 -0700

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,63 +1,42 @@
 #!/usr/bin/python3
 """
-parse log
+Log parsing
 """
+
 import sys
-import signal
-import re
 
-data = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0, 'filesize': 0}
-line_count = 0
+if __name__ == '__main__':
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
-def print_data(data):
-    """ print data """
-    print(f"File size: {data['filesize']}")
-    for k in sorted(data.keys()):
-        if isinstance(k, int) and data[k] != 0:
-            print(f"{k}: {data[k]}")
-
-def signal_handler(sig, frame):
-    """ signale handler """
-    print_data(data)
-    sys.exit(0)
-
-# Register the signal handler
-signal.signal(signal.SIGINT, signal_handler)
-
-def get_data():
-    """ get data """
-    global line_count
-    possible_status = {200, 301, 400, 401, 403, 404, 405, 500}
-
-    for line in sys.stdin:
-        parts = line.split(' ')
-        filesize = parts[-1]
-        status = parts[-2]
-
+    def process_line(line: str) -> None:
+        global filesize, count
+        count += 1
+        data = line.split()
         try:
-            status = int(status)
-        except ValueError:
-            continue
+            status_code = data[-2]
+            if status_code in stats:
+                stats[status_code] += 1
+        except IndexError:
+            pass
+        try:
+            filesize += int(data[-1])
+        except (IndexError, ValueError):
+            pass
+        if count % 10 == 0:
+            print_stats(stats, filesize)
 
-        if status not in possible_status:
-            continue
-
-        data[status] += 1
-        data['filesize'] += int(filesize)
-        line_count += 1
-
-        if line_count == 10:
-            print_data(data)
-                line_count = 0
-        else:
-            continue
-
-    if line_count > 0:
-        print_data(data)
-    elif line_count == 0:
-        print(f"File size: 0")
-
-if __name__ == "__main__":
-    get_data()
-
+    try:
+        for line in sys.stdin:
+            process_line(line)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
