e00610371ed58e5108ca3a89501dff72d59e554f
oussama-el1 <elhadraoui284@gmail.com>
2024-06-19 13:59:32 +0100

    slve 2

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -16,7 +16,7 @@ def makeChange(coins: List[int], total: int) -> int:
     dp = [total + 1] * (total + 1)
     dp[0] = 0
 
-    for i in range(coin, total + 1):
+    for i in range(1, total + 1):
         for coin in coins:
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
