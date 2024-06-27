f180102e8153df257e6ecc1a270be315b91ab942
oussama-el1 <elhadraoui284@gmail.com>
2024-06-27 23:13:58 +0100

    .\0-island_perimeter.py

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+island primeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    island_perimeter
+      grid : list of list of int
+    """
+    if not grid:
+        return 0
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                perimeter += 4
+                if r > 0 and grid[r-1][c] == 1:
+                    perimeter -= 2
+                if c > 0 and grid[r][c-1] == 1:
+                    perimeter -= 2
+    return perimeter
