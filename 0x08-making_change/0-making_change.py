e5ca3dab4192c9898b527f13fb37a59112bccd69
oussama-el1 <elhadraoui@284@gmail.com>
2024-07-24 18:56:54 +0100

    solve making chanfe

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""
-The coin change problem
-"""
+""" 0-making_change.py """
 from typing import List
 
 
 def makeChange(coins: List[int], total: int) -> int:
     """
-    makeChange Function
+    function: makeChange
+    args: coins -> List[int], total: int
     """
-
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    sorted_coins = sorted(coins, reverse=True)
+    counter = 0
+    copy_total = total
 
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+    for coin in sorted_coins:
+        if coin <= copy_total:
+            count = copy_total // coin
+            copy_total -= coin * count
+            counter += count
+        if copy_total == 0:
+            break
 
-    return dp[total] if dp[total] != float('inf') else -1
+    if copy_total != 0:
+        return -1
+
+    return counter
--- 0x08-making_change/test.py
+++
@@ -1,3 +0,0 @@
-coins = [2,4,5,32,6,255,5,42]
-coins.sort()
-print("Now it is sorted:", coins)
